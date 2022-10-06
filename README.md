7ea8b51d200a86252786617ff36c66fff95ee5a6
Binita Gupta <binitagupta24@gmail.com>
2022-10-05 23:22:22 -0400

    Add extra functionality, add comments, and update README

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # todo-app-flask
 A simple ToDo app built with Flask.
+
+## cd into folder
+```
+cd todo-app-flask
+```
+
+## Make sure you have Flask and SQL installed.
+```
+pip install Flask
+pip install Flask-SQLAlchemy
+```
+
+## Run the app
+```
+python app.py
+```
+Now you can go to http://127.0.0.1:5000/ to see the app running.
+
+## Extra functionality
+The extra functionality added was the "Update Priority" button, which adds a "Star" for important tasks, or removes it when checked again.
+
+## Citations
+All code is from https://www.python-engineer.com/posts/flask-todo-app/ 
--- app.py
+++ app.py
@@ -1,35 +1,45 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
+# Create Flask app
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Set up SQL database
 db = SQLAlchemy(app)
 
+# Create todo item model with attributes
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
+    starred = db.Column(db.Boolean)
 
-
+# Homepage route
 @app.route('/')
 def index():
     # Show all todos
     todo_list = Todo.query.all()
     return render_template('base.html', todo_list = todo_list)
 
+# When adding a new task, call POST method from base.html
 @app.route('/add', methods=["POST"])
 def add():
     # Add new item
     title = request.form.get("title")
-    new_todo = Todo(title=title, complete=False)
+
+    # Initialize todo
+    new_todo = Todo(title=title, complete=False, starred=False)
+
+    # Add todo to database
     db.session.add(new_todo)
     db.session.commit()
     return redirect(url_for("index"))
 
 @app.route('/update/<int:todo_id>')
 def update(todo_id):
-    # Add new item
+    # Update item as completed or not
     todo = Todo.query.filter_by(id=todo_id).first()
     todo.complete = not todo.complete
     db.session.commit()
@@ -37,23 +47,23 @@ def update(todo_id):
 
 @app.route('/delete/<int:todo_id>')
 def delete(todo_id):
-    # Add new item
+    # Delete item
     todo = Todo.query.filter_by(id=todo_id).first()
     db.session.delete(todo)
-    todo.complete = not todo.complete
     db.session.commit()
     return redirect(url_for("index"))
 
-@app.route('/about')
-def about():
-    return "About"
+@app.route('/star/<int:todo_id>')
+def star(todo_id):
+    # Mark item as important
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.starred = not todo.starred
+    db.session.commit()
+    return redirect(url_for("index"))
 
+# Set up database
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-            
-        new_todo = Todo(title="todo 1", complete=False)
-        db.session.add(new_todo)
-        db.session.commit()
 
     app.run(debug=True)
